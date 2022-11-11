9f72dbbd35410f9e037d0c15e39205252c4ab542
Spham117 <sean.pham117@gmail.com>
2022-11-10 20:50:22 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+key=0
+encr=0
+decr=0
+alg=0
+out=0
+msg=0
+#WILL CHANGE VARIABLE TYPE LATER!!!!!!11111!!!111!!!!!111!!!!
+isEncrypt = False
+
+def rkg():
+
+def encrypt(msg,key):
+
+def decrypt(key,msg):
+
+
