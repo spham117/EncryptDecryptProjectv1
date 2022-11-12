a3893e3b01ce954f9133bd53d0a7e74c552cfb16
Spham117 <sean.pham117@gmail.com>
2022-11-11 21:05:39 -0800

    Added a couple more variables and found out about the .split functionality to turn String type into an Array.

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ encr=0
 decr=0
 alg=0
 out=0
-msg=0
+msg = 0
 #WILL CHANGE VARIABLE TYPE LATER!!!!!!11111!!!111!!!!!111!!!!
 isEncrypt = False
-
 def rkg():
     value = random.randint(100,100000)
     return value
+key = rkg()
 def encrypt(msg,key):
-
+    
 def decrypt(key,msg):
 
-If isEncrypt = True:
+if isEncrypt:
+    print(key)
+UserInput = input('Type your message here')
+
+msg = UserInput.split()
+
+
+
+
 
 
