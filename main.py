fae16f56ed5a95b67a320ab2b0df5ccce585263c
Spham117 <sean.pham117@gmail.com>
2023-09-26 22:28:44 -0700

    changed for making decryption easier

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ string = input("enter string: ")
 shift = input("enter shift coefficient(integers): ")
 coefficient = int(shift)
 for i in string:
-    x = ord(i) * (((coefficient)**2)+3)
+    x = (ord(i) * abs(coefficient))+1
     y = str(x)
     z = ''.join(y)
-    print (z, end="")
+    print (z, end=" ")
