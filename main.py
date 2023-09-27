51aac3534e8574eaba137b67de49e0b3b1b5867b
Spham117 <sean.pham117@gmail.com>
2023-09-26 22:54:28 -0700

    added hex

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 string = input("enter string: ")
 shift = input("enter shift coefficient(integers): ")
 coefficient = int(shift)
+stored_var = ""
 for i in string:
     x = ((ord(i)**2 * abs(coefficient**2))+ coefficient + 1)+ ord(i)*2
     y = str(x)
-    print (hex(y), end="")
+    stored_var +=y
+    z = int(stored_var)
+    t = hex(z)
+    print (y, end=" ")
+    print(z)
+    print (t)
