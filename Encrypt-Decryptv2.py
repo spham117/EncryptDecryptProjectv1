d08a888e657c5250aab1be66839244d53587a61c
Spham117 <sean.pham117@gmail.com>
2023-09-26 21:48:07 -0700

    made the code more concise

--- Encrypt-Decryptv2.py
+++ Encrypt-Decryptv2.py
@@ -1,8 +1,8 @@
 string = input("enter string: ")
 shift = input("enter shift coefficient(integers): ")
-coeff = int(shift)
+coefficient = int(shift)
 for i in string:
-    x = ord(i) * (((coeff)*(coeff))+3)
+    x = ord(i) * (((coefficient)**2)+3)
     y = str(x)
     z = ' '.join(y)
     print (z, end=" ")
