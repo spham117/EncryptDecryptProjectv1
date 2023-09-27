b11b082c0bd2bc323883ff73a0b380ff7d85ef7a
Spham117 <sean.pham117@gmail.com>
2023-09-27 00:22:58 -0700

    fixed the code to make it a single number

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ string = input("enter string: ")
 shift = input("enter shift coefficient(integers): ")
 coefficient = int(shift)
 stored_var = ""
-for i in string:
-    x = ((ord(i)**2 * abs(coefficient**2))+ coefficient + 1)+ ord(i)*2
-    y = str(x)
-    stored_var +=y
-    z = int(stored_var)
-    t = hex(z)
-    print (y, end=" ")
-    print(z)
-    print (t)
+def encrypt(string,coefficient,shift,stored_var):
+    for i in string:
+        x = ((ord(i) * abs(coefficient**2))+ coefficient)+ ord(i)
+        y = str(x)
+        stored_var +=y
+    return(int(stored_var))
+print(hex(encrypt(string,coefficient,shift,stored_var)))
+print(encrypt(string,coefficient,shift,stored_var))
