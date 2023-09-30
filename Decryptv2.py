478298f2854e4a541462be9670ecf7a6299f4195
Sean P <sean.pham117@gmail.com>
2023-09-29 17:42:09 -0700

    added zfill and inverse functions

--- Decryptv2.py
+++ Decryptv2.py
@@ -4,7 +4,7 @@ import sys
 
 
 encoded_input = input("enter code: ").rstrip(',')
-byte_list = encoded_input.split(sep=",",maxsplit=-1)
+byte_list = encoded_input[::-1].split(sep=",",maxsplit=-1)
 byte_list1 = list(map(int,byte_list))
 shift = input("enter shift coefficient(integers): ")
 
@@ -12,9 +12,8 @@ if not re.match("\d+", shift):
     print ("Error! Only integers allowed!")
 
 else:
-    #coefficient = int(shift)
     pend_list = byte_list1
-    pend_list1 = [(val+int(shift)**2)//(int(shift)**3) for val in pend_list]
+    pend_list1 = [(val+int(shift)**3)//(int(shift)**3) for val in pend_list]
     pend_list2 = list(map(chr,pend_list1))
     list_join = ''.join(pend_list2)
-    print(list_join)
+    print(list_join[::-1])
--- encrypt.py
+++ encrypt.py
@@ -4,11 +4,11 @@ shift = input("enter shift coefficient(integers): ")
 coefficient = int(shift)
 
 for i in string:
-    x = ord(i) * coefficient**3 - coefficient**2
+    x = (ord(i) * coefficient**3) - coefficient**3
     y = str(x)
-    z = ''.join(y)
-    #print ((hex(int(z))), end=" ")
-    print(z, end=',')
+    a = y.zfill(10)
+    z = ''.join(a[::-1])
+    print(z ,end=',')
     #remember to omit the trailing comma after retrieving the output
     #This for loop will first encode the user's string input value to UTF-8 integer value then will multiply the integer
     #value with a user defined coefficient integer, which would be a way of scrambling the data(even though this is a very basic method of doing so)
