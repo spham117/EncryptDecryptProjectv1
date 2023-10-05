1d6d65ab2cde66d1208a92d4e1213e68432f899a
Sean P <sean.pham117@gmail.com>
2023-10-04 17:19:09 -0700

    Fixed issues with the cipher

--- Decryptv2.py
+++ Decryptv2.py
@@ -4,16 +4,21 @@ import sys
 
 
 encoded_input = input("enter code: ").rstrip(',')
-byte_list = encoded_input[::-1].split(sep=",",maxsplit=-1)
-byte_list1 = list(map(int,byte_list))
+
 shift = input("enter shift coefficient(integers): ")
 
+coefficient1 = ((encoded_input.count(',')+1) ** int(shift))
+
+byte_list = encoded_input.split(sep=",",maxsplit=-1)
+byte_list1 = list(map(int,byte_list))
+byte_list2 = [item//coefficient1 for item in byte_list1]
+
 if not re.match("\d+", shift):
-    print ("Error! Only integers allowed!")
+     print ("Error! Only integers allowed!")
 
 else:
-    pend_list = byte_list1
-    pend_list1 = [(val+int(shift)**3)//(int(shift)**3) for val in pend_list]
-    pend_list2 = list(map(chr,pend_list1))
-    list_join = ''.join(pend_list2)
-    print(list_join[::-1])
+     pend_list = byte_list2
+     pend_list1 = [((val+int(shift)**3)//(int(shift)**3)) for val in pend_list]
+     pend_list2 = list(map(chr,pend_list1))
+     list_join = ''.join(pend_list2)
+     print(list_join)
--- encrypt.py
+++ encrypt.py
@@ -2,14 +2,16 @@
 string = input("enter string: ")
 shift = input("enter shift coefficient(integers < 50): ")
 coefficient = int(shift)
+even_indices = []
+
 if coefficient <= 50:
     for i in string:
-        x = (ord(i) * coefficient**3) - coefficient**3
-        y = str(x)
-        a = y.zfill(10)
-        z = ''.join(a[::-1])
-        print(z ,end=',')
-        #remember to omit the trailing comma after retrieving the output
+        x = ((ord(i) * coefficient**3) - coefficient**3) * (((len(string)**coefficient)))
+        y = str(x+2)
+        z = ''.join(y)
+        print((z),end=',')
+
+
         #This for loop will first encode the user's string input value to UTF-8 integer value then will multiply the integer
         #value with a user defined coefficient integer, which would be a way of scrambling the data(even though this is a very basic method of doing so)
 else:
