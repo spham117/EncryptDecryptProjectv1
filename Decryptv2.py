96b49e836a04029ec2977282a176c3b0a65441e1
Sean P. <sean.pham117@gmail.com>
2023-10-09 15:29:33 -0700

    Added a loop to both encrypt and decrypt

--- Decryptv2.py
+++ Decryptv2.py
@@ -2,25 +2,25 @@ import math
 import re
 import sys
 
+while input != '':
+     encoded_input = input("enter code: ").rstrip(',')
+     if not re.match("\d+", encoded_input):
+          print ("Error! Only integers allowed!")
+     shift = input("enter shift coefficient(integers): ")
 
-encoded_input = input("enter code: ").rstrip(',')
-if not re.match("\d+", encoded_input):
-     print ("Error! Only integers allowed!")
-shift = input("enter shift coefficient(integers): ")
+     coefficient1 = ((encoded_input.count(',')+1) * int(shift))
 
-coefficient1 = ((encoded_input.count(',')+1) * int(shift))
+     byte_list = encoded_input.split(sep=",",maxsplit=-1)
+     byte_list1 = list(map(int,byte_list))
+     byte_list2 = [(item + coefficient1)+(int(shift)**3) for item in byte_list1]
 
-byte_list = encoded_input.split(sep=",",maxsplit=-1)
-byte_list1 = list(map(int,byte_list))
-byte_list2 = [(item + coefficient1)+(int(shift)**3) for item in byte_list1]
-
-if not re.match("\d+", shift):
-     print ("Error! Only integers allowed!")
-try:
-     pend_list = byte_list2
-     pend_list1 = [((val+int(shift)**3)//(int(shift)**3)) for val in pend_list]  
-     pend_list2 = list(map(chr,pend_list1))
-     list_join = ''.join(pend_list2)
-     print(list_join)
-except:
-    print("An error has occurred. Please try again.")
+     if not re.match("\d+", shift):
+          print ("Error! Only integers allowed!")
+     try:
+          pend_list = byte_list2
+          pend_list1 = [((val+int(shift)**3)//(int(shift)**3)) for val in pend_list]  
+          pend_list2 = list(map(chr,pend_list1))
+          list_join = ''.join(pend_list2)
+          print(list_join)
+     except:
+          print("An error has occurred. Please try again.")
--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,18 @@
 
-string = input("enter string: ")
-shift = input("enter shift coefficient(integers =< 31): ")
-coefficient = abs(int(shift))
+while input != "":
+    string = input("enter string: ")
+    shift = input("enter shift coefficient(integers =< 31): ")
+    coefficient = abs(int(shift))
 
-if coefficient <= 31:
-    for i in string:
-        x = (((ord(i) * coefficient**3) - coefficient**3) - (((len(string)*coefficient))))- coefficient**3
-        y = str(x)
-        z = ''.join(y)
-        print((z),end=',')
+    if coefficient <= 31:
+        for i in string:
+            x = (((ord(i) * coefficient**3) - coefficient**3) - (((len(string)*coefficient))))- coefficient**3
+            y = str(x)
+            z = ''.join(y)
+            print((z),end=',')
 
 
-        #This for loop will first encode the user's string input value to UTF-8 integer value then will multiply the integer
-        #value with a user defined coefficient integer, which would be a way of scrambling the data(even though this is a very basic method of doing so)
-else:
-    print("READ THE INSTRUCTIONS PLEASE >:(")
+            #This for loop will first encode the user's string input value to UTF-8 integer value then will multiply the integer
+            #value with a user defined coefficient integer, which would be a way of scrambling the data(even though this is a very basic method of doing so)
+    else:
+        print("READ THE INSTRUCTIONS PLEASE >:(")
